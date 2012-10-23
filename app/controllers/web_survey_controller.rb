9e48e83a8c5ff8f42834308cfca967e39b7bc6a0
岩谷 昭宏 <aiwaya@iwaya-no-MacBook-Air.local>
2012-10-23 12:35:23 +0900

    s

--- app/controllers/web_survey_controller.rb
+++ app/controllers/web_survey_controller.rb
@@ -2,11 +2,10 @@ class WebSurveyController < ApplicationController
   def index
     client = Databasedotcom::Client.new :client_id => "3MVG98XJQQAccJQd99vjXysbDsJLHGJdGvSwasAUAvNOnGsAKs0qymACgYSwvC9_9szDukaHitpZR3J4sjOIe", :client_secret => "1503327172143669282", :host=> "login.salesforce.com"
     client.authenticate(:username => "websurvey@1.com", :password => "abcd1234")
+    client.create('websurvey__c', 'q1__c' => 'sdsd')
 
-    survey_class = client.materialize("websurvey__c")
-    survey = survey_class.new
-    survey.q1__c = 'yes'
-    survey.save
+
   end
+
 
 end
