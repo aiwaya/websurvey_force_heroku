edd5b6a1ead184187432f4070fa8704c8bf6745d
岩谷 昭宏 <aiwaya@iwaya-no-MacBook-Air.local>
2012-10-24 09:33:22 +0900

    a

--- app/controllers/web_survey_controller.rb
+++ app/controllers/web_survey_controller.rb
@@ -2,9 +2,10 @@ class WebSurveyController < ApplicationController
   def index
 
 
-    client = Databasedotcom::Client.new :client_id => "3MVG98XJQQAccJQd99vjXysbDsJLHGJdGvSwasAUAvNOnGsAKs0qymACgYSwvC9_9szDukaHitpZR3J4sjOIe", :client_secret => "1503327172143669282", :host=> "login.salesforce.com"
+    client = Databasedotcom::Client.new :client_id => "3MVG98XJQQAccJQd99vjXysbDsJLHGJdGvSwasAUAvNOnGsAKs0qymACgYSwvC9_9szDukaHitpZR3J4sjOIe", :client_secret => "1503327172143669282"
     client.authenticate(:username => "websurvey@1.com", :password => "abcd1234")
-    client.create('websurvey__c', 'q1__c' => 'sdsd')
+    client.
+    #client.create('websurvey__c', 'q1__c' => 'sdsd')
 
   end
 
