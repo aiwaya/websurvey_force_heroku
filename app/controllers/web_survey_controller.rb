0f6c0cc0ea63cf55dad201e3e88a025a5e7fdf9c
岩谷 昭宏 <aiwaya@iwaya-no-MacBook-Air.local>
2012-10-24 09:36:51 +0900

    a

--- app/controllers/web_survey_controller.rb
+++ app/controllers/web_survey_controller.rb
@@ -4,7 +4,7 @@ class WebSurveyController < ApplicationController
 
     client = Databasedotcom::Client.new :client_id => "3MVG98XJQQAccJQd99vjXysbDsJLHGJdGvSwasAUAvNOnGsAKs0qymACgYSwvC9_9szDukaHitpZR3J4sjOIe", :client_secret => "1503327172143669282"
     client.authenticate(:username => "websurvey@1.com", :password => "abcd1234")
-    client.
+
     #client.create('websurvey__c', 'q1__c' => 'sdsd')
 
   end
