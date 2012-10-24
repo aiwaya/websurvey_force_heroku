c9ae39fb9242491c6ed87b01b92a5167318abc86
岩谷 昭宏 <aiwaya@iwaya-no-MacBook-Air.local>
2012-10-24 09:40:12 +0900

    a

--- app/controllers/web_survey_controller.rb
+++ app/controllers/web_survey_controller.rb
@@ -1,11 +1,16 @@
 class WebSurveyController < ApplicationController
   def index
 
+    client = Databasedotcom::Client.new :client_id => "3MVG9lKcPoNINVBL1iUvM1GVTMDlSGNB4nMCX5IU9Ww1dnQHBC.YMkaH_8EnRDkgGrfdePK1Oe8rUCGMoZd6B", :client_secret => "2405887836979254114", :host=> "prerellogin.pre.salesforce.com"
+    client.authenticate(:username => "aiwaya@gmail.com", :password => "abcd1234")
 
+    @objs = client.list_sobjects
+=begin
     client = Databasedotcom::Client.new :client_id => "3MVG98XJQQAccJQd99vjXysbDsJLHGJdGvSwasAUAvNOnGsAKs0qymACgYSwvC9_9szDukaHitpZR3J4sjOIe", :client_secret => "1503327172143669282"
     client.authenticate(:username => "websurvey@1.com", :password => "abcd1234")
 
-    #client.create('websurvey__c', 'q1__c' => 'sdsd')
+=end
+#client.create('websurvey__c', 'q1__c' => 'sdsd')
 
   end
 
