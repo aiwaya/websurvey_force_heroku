38c8559cd5f19111ff15e6094664546a249540e8
岩谷 昭宏 <aiwaya@iwaya-no-MacBook-Air.local>
2012-12-04 12:52:07 +0900

    a

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,13 @@ require File.expand_path('../application', __FILE__)
 Websurvey::Application.initialize!
 
 ################################# Please change the following parameters #######################################
-ENV['sfdc_host'] = 'na11.salesforce.com'
+ENV['sfdc_host'] = 'xxx.salesforce.com'
 ENV['sfdc_api_version'] = 'v23.0'
-ENV['sfdc_client_id'] = '3MVG98XJQQAccJQd99vjXysbDsB_kw1g_8TaIgKSOz_pUb4FSx8iWoOoKI6dPQdvpMA3t4LJWbGAmGjR24Fa6'
-ENV['sfdc_client_secret'] = '637800270968105395'
+ENV['sfdc_client_id'] = 'xxxxxxxx'
+ENV['sfdc_client_secret'] = 'xxxxxxxx'
 
-ENV['sfdc_password'] = 'abcd1234'
-ENV['sfdc_user'] = 'websurvey@1.com'
+ENV['sfdc_password'] = 'xxxxxx'
+ENV['sfdc_user'] = 'xxxxxx@xxxx.com'
 
 
 
